7b8bf7f4e1789ef114edcd758b239b2fa009cdb3
Hardson <hardsontessi2@gmail.com>
2022-04-08 05:22:42 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int n, tmp, a;
+
+	for (n = 48; n < 63; n++)
+	{
+		if (n > 57)
+		{
+			tmp = n - 57;
+			a = 97 + tmp;
+			putchar(a);
+		}
+		else
+		{	
+			putchar(n);															 }
+	}
+	putchar('\n');
+	return (0);
+}
