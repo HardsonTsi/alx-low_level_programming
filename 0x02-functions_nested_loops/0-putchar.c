b51357cedeb8e86fa0b5a548b3e1887720daa458
Hardson <hardsontessi2@gmail.com>
2022-04-12 03:08:36 -0700

    initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,10 +8,10 @@
 */
 int main(void)
 {
-	char word = "_putchar";
+	char word[9] = "_putchar";
 	int i = 0;
 
-	for (i = 0; i = < strlen(word); i++)
+	for (i = 0; i < 9; i++)
 		_putchar(word[i]);
 
 	_putchar('\n');
