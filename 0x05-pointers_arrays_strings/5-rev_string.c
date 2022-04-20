6a98d3c153b42d46e892a1b9c0f278a20850c149
Your Name <hardsontessi2@gmail.com>
2022-04-20 19:49:58 +0100

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,17 +7,16 @@
  *
  * Return: nothing
  */
-
 void rev_string(char *s)
 {
-    int i, tmp, len = _strlen(s);
+int i, tmp, len = _strlen(s);
 
-    for (i = 0; i < len / 2; i++)
-    {
-        tmp = *(s + i);
-        *(s + i) = *(s + len - i - 1);
-        *(s + len - i - 1) = tmp;
-    }
+for (i = 0; i < len / 2; i++)
+{
+tmp = *(s + i);
+*(s + i) = *(s + len - i - 1);
+*(s + len - i - 1) = tmp;
+}
 }
 
 /**
@@ -29,10 +28,10 @@ void rev_string(char *s)
 
 int _strlen(char *s)
 {
-    int len = 0;
+int len = 0;
 
-    while (*(s + len) != '\0')
-        len++;
+while (*(s + len) != '\0')
+    len++;
 
-    return (len);
+return (len);
 }
