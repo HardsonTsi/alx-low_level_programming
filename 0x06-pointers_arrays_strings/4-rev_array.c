4cd99c7d1fcc9ee5241a90beef06d297d13b5c30
Your Name <hardsontessi2@gmail.com>
2022-04-22 13:41:05 +0100

    reverse_aray

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -29,15 +29,16 @@ void print_array(int *a, int n)
 void reverse_array(int *a, int n)
 {
     int i;
-    int j = n - 1;
+    int j = n;
 
     int *aCopy;
 
-    for (i = 0; i < n; i++, j--)
+    for (i = 0; i < n; i++)
     {
+        j--;
 
-        *(aCopy + i) = *(a + j);
-        printf("%d", *(aCopy +i));
+        aCopy[i] = a[j];
+        printf("%d", aCopy[i]);
     }
 }
 
@@ -45,8 +46,8 @@ int main()
 {
     int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
-    print_array(a, sizeof(a) / sizeof(int));
+    // print_array(a, sizeof(a) / sizeof(int));
     reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
+    // print_array(a, sizeof(a) / sizeof(int));
     return (0);
 }
