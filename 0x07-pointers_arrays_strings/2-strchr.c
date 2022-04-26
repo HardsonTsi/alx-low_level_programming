73a1d235b32ba5c8affe40701c8742003d42f26c
Your Name <hardsontessi2@gmail.com>
2022-04-26 21:11:30 +0100

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,24 +12,12 @@ char *_strchr(char *s, char c)
 
     char *search = NULL;
 
-	while (*s != '\0')
+	while (*s != '\0' && *s != c)
 	{
 		if (*s == c) 
 		{
 			search = s; 
 		}
 		s++;
-	}
-<<<<<<< HEAD
-	return (search);
-=======
-
-	if (*s == c)
-	{
-		return (s);
-	}
-
-
-	return (0);
->>>>>>> e5233573bcac7287ce93b323effea46c29142660
+    }
 }
